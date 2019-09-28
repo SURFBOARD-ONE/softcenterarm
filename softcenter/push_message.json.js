4a9455575160d658678c5dda818214cbfb23936e
Xu Zhaowei <paldier@hotmail.com>
2019-09-29 00:37:59 +0800

    update push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,10 @@
 {
 "title":"<em>梅林固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b>软件中心：新版1.2.1已经更新，低版本固件可能会无法运行</b>",
+"content1":"<b>软件中心：新版1.2.1已经更新，低版本固件尽快更新,否则部分插件可能会无法运行</b>",
 "content2":"不兼容ks插件",
 "content3":"部分插件下载缓慢，5分钟后刷新查看",
-"content4":"启用kp后产生的任何后果和本固件及软件中心无关"
+"content4":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
+<a href='https://github.com/paldier/softcenter' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='https://t.me/merlinsc' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>"
 }
