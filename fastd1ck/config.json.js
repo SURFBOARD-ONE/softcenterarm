1b86c1124915a6bf626dc8b1006ac837dc1f6a12
Xu Zhaowei <paldier@hotmail.com>
2020-05-21 08:34:54 +0800

    update fastd1ck

--- fastd1ck/config.json.js
+++ fastd1ck/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.6",
-"md5":"dd858b291e026b9aca3c27cbea7bc431",
+"version":"1.7",
+"md5":"9798eb8578dad68e23222f8621ec2237",
 "home_url":"Module_fastd1ck.asp",
 "title":"讯雷快鸟",
 "description":"迅雷快鸟，为上网加速而生~",
-"build_date":"2019-07-25_11:34:22"
+"build_date":"2020-05-21_08:34:26"
 }
