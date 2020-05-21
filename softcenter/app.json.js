d8f40ea4de7b33076bab36fe94594c5a1dd60cad
Xu Zhaowei <paldier@hotmail.com>
2020-05-21 11:03:26 +0800

    repack unblockmusic

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,10 +1,10 @@
 {
     "apps": [
         {
-            "build_date": "2020-03-11_15:38:13", 
+            "build_date": "2020-05-21_11:03:01", 
             "description": "解锁网易云灰色歌曲", 
             "home_url": "Module_unblockmusic.asp", 
-            "md5": "091868675cb60395264b91757ad33060", 
+            "md5": "ca74951fcfb62affe3a51bf3c0e3e00e", 
             "name": "unblockmusic", 
             "tar_url": "unblockmusic/unblockmusic.tar.gz", 
             "title": "解锁网易云灰色歌曲", 
--- unblockmusic/config.json.js
+++ unblockmusic/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.0.2",
-"md5":"091868675cb60395264b91757ad33060",
+"md5":"ca74951fcfb62affe3a51bf3c0e3e00e",
 "home_url":"Module_unblockmusic.asp",
 "title":"解锁网易云灰色歌曲",
 "description":"解锁网易云灰色歌曲",
-"build_date":"2020-03-11_15:38:13"
+"build_date":"2020-05-21_11:03:01"
 }
