e69533d4c6d64afbc9b1065fd5ee9324908805bb
Xu Zhaowei <paldier@hotmail.com>
2020-06-22 09:12:50 +0800

    add linkease

---
+++ linkease/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"2.5",
+"md5":"095c1f69022931ed2df596a84175eea1",
+"home_url":"Module_linkease.asp",
+"title":"易有云2.0",
+"description":"易有云2.0 （LinkEase） 手机相册同步，PC 双向同步！",
+"build_date":"2020-06-11_10:56:47"
+}
