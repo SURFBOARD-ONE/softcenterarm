f7307fb5e2936f7e2e874a8b07884c95cdd56395
Xu Zhaowei <paldier@hotmail.com>
2021-07-20 16:14:07 +0800

    update

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>软件中心&nbsp;for&nbsp;arm&nbsp;platform</em>",
 "content1":"<b>软件中心：1.3.5已发布，同时支持api版本1.1和1.5</b>",
-"content2":"固件5.1.5:已发布,<font color='#1E90FF'>同时支持api版本1.1和1.5</font>",
+"content2":"<font color='#1E90FF'>bcm470x系列fullcone测试开始.想要参加测试的进群</font>",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/paldier/softcenterarm' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
