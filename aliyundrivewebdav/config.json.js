ce103ffbe5f80a9aeb0b6ca85cdccb9e0fa1ef0a
Xu Zhaowei <paldier@hotmail.com>
2021-11-16 19:25:51 +0800

    add aliyunwebdav

---
+++ aliyundrivewebdav/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.5.5",
+"md5":"6d2dc01ffddf5f4e18d25fc1d53271c0",
+"home_url":"Module_aliyundrivewebdav.asp",
+"title":"阿里云盘WebDAV",
+"description":"阿里云盘 WebDAV 服务器",
+"build_date":"2021-11-16_19:25:48"
+}
